c908853d6ce5d99eaa9b59b039fb7388160fb4ee
wafanakha <wafanakha15@gmail.com>
2023-11-09 22:51:18 +0700

    adding commiting the numbers and operators from calculator button

--- calc.js
+++ calc.js
@@ -1,8 +1,22 @@
-let operation = [];
-
+let operation = "";
+const buttons = document.querySelectorAll(".calculator button");
+buttons.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (
+      element.innerHTML == "*" ||
+      element.innerHTML == "/" ||
+      element.innerHTML == "-" ||
+      element.innerHTML == "+"
+    ) {
+      operation += " " + element.innerHTML + " ";
+    } else {
+      operation += element.innerHTML;
+    }
+    console.log(operation);
+  });
+});
 function operate() {
-  return eval(operation.join(" "));
+  return eval(operation);
 }
 
-console.log(operation);
 console.log(operate());
